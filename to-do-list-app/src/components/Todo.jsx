2b79bebe2fa06e9e4fa67cc35b940eff559b5e6b
Nina Henriette <ninahenrietterarr@gmail.com>
2023-05-19 14:14:51 +0200

    'commit'

---
+++ to-do-list-app/src/components/Todo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenSquare, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+    return (
+        <div className='Todo'>
+            <p onClick={() => toggleComplete(task.id)}
+            className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
+            <div className='icons'>
+                <FontAwesomeIcon className='icon1' icon={faPenSquare} onClick={() => editTodo(task.id)} />
+                <FontAwesomeIcon className='icon2' icon={faTrashAlt} onClick={() => deleteTodo(task.id)}/>
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
+
+
+
